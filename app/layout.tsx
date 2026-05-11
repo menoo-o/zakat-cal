1e3f30126e10329050c89874a826665e376a3f84
menoo-o <menosuper6@gmail.com>
2026-05-11 14:49:58 +0500

    zakat cal progress pt3.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import TopLoader from '@/components/top-loader'
+// import TopLoader from '@/components/top-loader'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-      <TopLoader />
+    
       {children}
       </body>
     </html>
